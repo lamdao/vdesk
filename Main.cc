27567ee4b5246ab6494e57917c32141b04ddc1aa
mrchuoi <mrchuoi>
2003-11-13 19:27:53 +0000

    Ignore SIGCLD

--- Main.cc
+++ Main.cc
@@ -1,16 +1,25 @@
+#include <signal.h>
 #include "Desktop.h"
 //----------------------------------------------------------------------------
 void init()
 {
-	/*
-	if( fork()>0 ) exit(0);
-	for( int i=0; i<256; i++ ) close(i);
+//	if( fork()>0 ) exit(0);
+	for( int i=3; i<256; i++ ) close(i);
 	setsid();
-	/ *
-	open("/dev/console", O_RDONLY);
-	open("/dev/console", O_WRONLY);
-	open("/dev/console", O_WRONLY);
-	*/
+/*
+#if 0
+	signal(SIGFPE, SIG_IGN);
+	signal(SIGSEGV, SIG_IGN);
+	signal(SIGILL, SIG_IGN);
+#endif
+	signal(SIGSYS, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTRAP, SIG_IGN);
+	signal(SIGIOT, SIG_IGN);
+	signal(SIGTERM, SIG_IGN);
+	signal(SIGHUP, SIG_IGN);
+*/	signal(SIGCHLD, SIG_IGN);
 }
 //----------------------------------------------------------------------------
 int main( int argc, char **argv )
